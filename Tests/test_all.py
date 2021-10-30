07e4bb5ea3cb84e4d07cd7d0e18f1e0eb375b127
Vlad Ivanescu <vlad.ivanescu@stud.ubbcluj.ro>
2021-10-30 16:47:33 +0300

    implemented 3.3

--- Tests/test_all.py
+++ Tests/test_all.py
@@ -1,6 +1,6 @@
 from Tests.test_CRUD import test_adauga_rezervare, test_sterge_rezervare, test_modifica_rezervare
 from Tests.test_domain import test_rezervare
-from Tests.test_functionality import test_trece_clasa_superioara
+from Tests.test_functionality import test_trece_clasa_superioara, test_ieftinire
 
 
 def run_all_tests():
@@ -9,3 +9,4 @@ def run_all_tests():
     test_sterge_rezervare()
     test_modifica_rezervare()
     test_trece_clasa_superioara()
+    test_ieftinire()
--- Tests/test_functionality.py
+++ Tests/test_functionality.py
@@ -1,6 +1,6 @@
-from Domain.rezervare import get_clasa
+from Domain.rezervare import get_clasa, get_pret
 from Logic.CRUD import adauga_rezervare
-from Logic.functionality import trece_clasa_superioara
+from Logic.functionality import trece_clasa_superioara, ieftinire
 
 
 def test_trece_clasa_superioara():
@@ -14,4 +14,16 @@ def test_trece_clasa_superioara():
     assert get_clasa(lista[0]) == "business"
     assert get_clasa(lista[1]) == "economy plus"
     assert get_clasa(lista[2]) == "business"
+
+
+def test_ieftinire():
+    lista = []
+    lista = adauga_rezervare("1", "Vlad", "business", 100, "da", lista)
+    lista = adauga_rezervare("2", "Emma", "economy plus", 80, "da", lista)
+    lista = adauga_rezervare("3", "Mark", "economy", 80, "nu", lista)
+    ieftinire(lista, 25)
+
+    assert get_pret(lista[0]) == 75
+    assert get_pret(lista[1]) == 60
+    assert get_pret(lista[2]) == 80
 
