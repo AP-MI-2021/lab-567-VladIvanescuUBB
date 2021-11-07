7f7353ae383395a2c4a7429fb1a01a8404dfa1a0
Vlad Ivanescu <vlad.ivanescu@stud.ubbcluj.ro>
2021-11-07 11:24:50 +0200

    implemented modificari lab6

--- UserInterface/console_2.py
+++ UserInterface/console_2.py
@@ -1,5 +1,6 @@
-from Domain.rezervare import to_string
 from Logic.CRUD import adauga_rezervare, sterge_rezervare, modifica_rezervare
+from Logic.functionality import trece_clasa_superioara, ieftinire
+from UserInterface.console import ui_pret_max_clasa, ui_ordonare_descrescator_pret, ui_suma_pret, show_all
 
 
 def print_menu_2():
@@ -12,35 +13,57 @@ def print_menu_2():
     print("3.3: Ieftinirea tuturor rezervărilor la care s-a făcut checkin cu un procentaj citit")
     print("3.4: Determinarea prețului maxim pentru fiecare clasă.")
     print("3.5: Ordonarea rezervărilor descrescător după preț.")
+    print("3.6:  Afișarea sumelor prețurilor pentru fiecare nume.")
+    print("undo: Undo")
     print("stop: Iesire")
 
 
-def ui_adauga_rezervare_2(ID, nume, clasa, pret, checkin_facut, lista):
-    return adauga_rezervare(ID, nume, clasa, pret, checkin_facut, lista)
-
-
-def show_all_2(lista):
-    for rezervare in lista:
-        print(to_string(rezervare))
-
-
-def ui_sterge_rezervare_2(ID, lista):
+def ui_adauga_rezervare_2(ID, nume, clasa, pret, checkin_facut, lista, undo_list):
     try:
-        return sterge_rezervare(ID, lista)
+        rezultat = adauga_rezervare(ID, nume, clasa, pret, checkin_facut, lista)
+        undo_list.append(lista)
+        return rezultat
     except ValueError as ve:
         print("Eroare: ", ve)
         return lista
 
 
-def ui_modifica_rezervare_2(ID, nume, clasa, pret, checkin_facut, lista):
+def ui_sterge_rezervare_2(ID, lista, undo_list):
     try:
-        return modifica_rezervare(ID, nume, clasa, pret, checkin_facut, lista)
+        rezultat = sterge_rezervare(ID, lista)
+        undo_list.append(lista)
+        return rezultat
     except ValueError as ve:
         print("Eroare: ", ve)
         return lista
+
+
+def ui_modifica_rezervare_2(ID, nume, clasa, pret, checkin_facut, lista, undo_list):
+    try:
+        rezultat = modifica_rezervare(ID, nume, clasa, pret, checkin_facut, lista)
+        undo_list.append(lista)
+        return rezultat
+    except ValueError as ve:
+        print("Eroare: ", ve)
+        return lista
+
+
+def ui_trece_clasa_superioara_2(lista, nume, undo_list):
+    undo_list.append(lista)
+    return trece_clasa_superioara(lista, nume)
+
+
+def ui_ieftinire_2(lista, procentaj, undo_list):
+    try:
+        rezultat = ieftinire(lista, procentaj)
+        undo_list.append(lista)
+        return rezultat
+    except ValueError as ve:
+        print("Eroare! ", ve)
 
 
 def run_menu_2(lista):
+    undo_list = []
     while True:
         cerinta = input("Dati cerinta: ")
         if cerinta == "stop":
@@ -54,22 +77,37 @@ def run_menu_2(lista):
                     ID = c.split(",")[1]
                     nume = c.split(",")[2]
                     clasa = c.split(",")[3]
-                    pret = c.split(",")[4]
+                    pret = float(c.split(",")[4])
                     checkin_facut = c.split(",")[5]
-                    lista = ui_adauga_rezervare_2(ID, nume, clasa, pret, checkin_facut, lista)
+                    lista = ui_adauga_rezervare_2(ID, nume, clasa, pret, checkin_facut, lista, undo_list)
                 elif c[0:4] == "show":
-                    show_all_2(lista)
+                    show_all(lista)
                 elif c[0:6] == "delete":
                     ID = c.split(",")[1]
-                    lista = ui_sterge_rezervare_2(ID, lista)
+                    lista = ui_sterge_rezervare_2(ID, lista, undo_list)
                 elif c[0:6] == "modify":
                     ID = c.split(",")[1]
                     nume = c.split(",")[2]
                     clasa = c.split(",")[3]
                     pret = c.split(",")[4]
                     checkin_facut = c.split(",")[5]
-                    lista = ui_modifica_rezervare_2(ID, nume, clasa, pret, checkin_facut, lista)
+                    lista = ui_modifica_rezervare_2(ID, nume, clasa, pret, checkin_facut, lista, undo_list)
+                elif c[0:3] == "3.2":
+                    nume = c.split(",")[1]
+                    lista = ui_trece_clasa_superioara_2(lista, nume, undo_list)
+                elif c[0:3] == "3.3":
+                    procentaj = float(c.split(",")[1])
+                    lista = ui_ieftinire_2(lista, procentaj, undo_list)
+                elif c[0:3] == "3.4":
+                    ui_pret_max_clasa(lista)
+                elif c[0:3] == "3.5":
+                    lista = ui_ordonare_descrescator_pret(lista, undo_list)
+                elif c[0:3] == "3.6":
+                    ui_suma_pret(lista)
+                elif c[0:4] == "undo":
+                    if len(undo_list) > 0:
+                        lista = undo_list.pop()
+                    else:
+                        print("Nu se poate face Undo")
                 else:
                     print("Cerinta inexistenta! Mai incercati")
-
-
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from Tests.test_all import run_all_tests
-from UserInterface.console import run_menu
+# from UserInterface.console import run_menu
 from UserInterface.console_2 import run_menu_2  # modificari lab6
 
 
 def main():
     run_all_tests()
-    run_menu([])
-    # run_menu_2([])  # modificari lab6
+    # run_menu([])
+    run_menu_2([])  # modificari lab6
 
 
 main()
