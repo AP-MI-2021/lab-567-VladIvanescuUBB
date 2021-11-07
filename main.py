e81321a7b2e09a52652486c3c6499dc02c220313
Vlad Ivanescu <vlad.ivanescu@stud.ubbcluj.ro>
2021-11-07 11:39:23 +0200

    implemented modificari lab6

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from Tests.test_all import run_all_tests
-# from UserInterface.console import run_menu
-from UserInterface.console_2 import run_menu_2  # modificari lab6
+from UserInterface.console import run_menu
+from UserInterface.console_2 import run_menu_2
 
 
 def main():
     run_all_tests()
-    # run_menu([])
-    run_menu_2([])  # modificari lab6
+    optiune = input("Old menu or New menu? (old/new): ")
+    if optiune == "old":
+        run_menu([])
+    else:
+        run_menu_2([])
 
 
 main()
